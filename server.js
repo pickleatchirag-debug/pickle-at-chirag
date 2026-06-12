692f8750fda3ab9eca51dac028be191c058c9ea7
pickleatchirag-debug <pickleatchirag@gmail.com>
2026-06-12 10:26:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.static(__dirname));
 
 // 🎯 SECURE ACTIVE GOOGLE WEB APP EXEC LINK STRINGS
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_elXPrUxfCPl1WYiPx2gc6TWpohNY-osHhfGgxeZBacn1vimm433n7sHUx2AvuVvHtg/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby_elXprUxfCPl1WYiPx2gc6TWpohNY-osHhfGgxeZBacn1vimm433n7sHUx2AvuVvHtg/exec";
 
 // Master memory storage registries synced directly to your spreadsheet
 let REGISTERED_USERS = [];
